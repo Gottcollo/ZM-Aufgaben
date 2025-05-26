806d23fcef3d10777070e109a2cb14a64ebaf97b
Gottcollo <gottcollo@gmail.com>
2025-05-26 11:37:47 +0200

    Kommentiert

--- AUfgaben/Operatorentesting.py
+++ AUfgaben/Operatorentesting.py
@@ -7,4 +7,7 @@ print(value1 == value2)#hier auf glecihehit überprüft
 print('jetzt ein typecheck bei 3<=5 ')
 print(type(3<=5))#hier ein typecheck
 print(3<=5)
-
+print('Value3 check geteilt durch 2')#hier ein dritten value hergestellt und es mit value3 / 2 ausgeben
+value3 = value1 + value2
+value3 /= 2
+print(value3)
