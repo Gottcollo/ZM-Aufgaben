2fd76b4aeec08d30f8f99e29e6a978b514a0d7dd
Gottcollo <gottcollo@gmail.com>
2025-05-26 09:31:13 +0200

    Kommentiert

--- AUfgaben/Testlauf.py
+++ AUfgaben/Testlauf.py
@@ -1,5 +1,9 @@
 print('Geburt von mir')
-name = input('Wie heißen wir?')
+
+name = input('Wie heißen wir?')#bei input wird keine Überprüfung gemacht ob der User wirklich ein String angibt
+
 print('Unser Name ist ' + name)
-age = input('Wie alt sind wir?')
+
+age = input('Wie alt sind wir?')#keine Überprüfung ob der User eine Float/Integer Zahl angibt
+
 print(F'Wir sind {age} Jahre alt')
