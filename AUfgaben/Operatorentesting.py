2d3792ae3c1431590751561777dad619a7a69195
Gottcollo <gottcollo@gmail.com>
2025-05-26 11:34:01 +0200

    AddiererplusBoolcheck

---
+++ AUfgaben/Operatorentesting.py
@@ -0,0 +1,10 @@
+value1 = float(input('gib deine erste zahl an: '))#hier ein typecast zu float
+value2 = float(input('deine zweite zahl: '))#typecast zu float damit kein str beginnt
+
+print(value1 + value2)#check ob es richtig rechnet
+print('Boolcheck beider werte')
+print(value1 == value2)#hier auf glecihehit überprüft
+print('jetzt ein typecheck bei 3<=5 ')
+print(type(3<=5))#hier ein typecheck
+print(3<=5)
+
--- AUfgaben/babyaddierer.py
+++
@@ -1,4 +0,0 @@
-value1 = float(input('gib deine erste zahl an: '))
-value2 = float(input('deine zweite zahl: '))
-
-print(value1 + value2)
