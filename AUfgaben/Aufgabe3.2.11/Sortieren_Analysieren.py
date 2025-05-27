9f22b599ec5a90494efab1085566754cff7a5622
Gottcollo <gottcollo@gmail.com>
2025-05-27 15:57:06 +0200

    es ist wieder falsch-.-

--- AUfgaben/Aufgabe3.2.11/Sortieren_Analysieren.py
+++ AUfgaben/Aufgabe3.2.11/Sortieren_Analysieren.py
@@ -8,29 +8,26 @@ while True:
         zahl = int(userinput) #für denn Bonus später jetzt schon INT anstatt Float
         zahlen.append(zahl) #append fügt einfach ein element hinzu 
                             #hier lass ich die zahl die der user gibt einfügen in die liste
-    except ValueError:
+        min = zahlen[0]
+        max = zahlen[0]
+        summe = 0
+
+        for zahl in zahlen:
+            if zahl < min:
+                min = zahl
+            if zahl > max:
+                max = zahl
+
+            summe += zahl
+
+        durchschnitt = summe /len(zahlen) #schon in listenzahlen genutzt
+        sortiert = sorted(zahlen)  #sorted ist eine funktion zum sortieren von INT
+
+        print(F'Maximale Zahl ist {max}')
+        print(F'Minimalste Zahl ist {min}')
+        print(F'Ein Sortierter Zahlenstrahl {sortiert}')
+        print(F'Noch mehr?')
+
+    except ValueError: #try und except sind schwer zu meistern
         print('Das war wohl keine Ganzzahl')
 
-if len(zahlen) != 0: #längencheck der liste sobald was drin ist gehts los
-    minimum = zahlen[0]
-    maximum = zahlen[0]
-    summe = 0
-
-    for zahl in zahlen:
-        if zahl < minimum:
-            minimum = zahl
-        if zahl > maximum:
-            maximum = zahl
-        summe += zahl
-
-    durchschnitt = summe / len(zahlen)
-
-    # Liste sortieren (aufsteigend)
-    zahlen.sort()
-    
-    print(f"Größte Zahl: {maximum}")
-    print(f"Kleinste Zahl: {minimum}")
-    print(f"Durchschnitt: {durchschnitt:.2f}")
-    print(f"Sortierte Liste: {zahlen}")
-else:
-    print("Keine Zahlen eingegeben.")
