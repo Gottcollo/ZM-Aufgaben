a2ea859a5550da37fed32810c2298a996ef72690
Gottcollo <gottcollo@gmail.com>
2025-05-27 14:00:50 +0200

    Testlauf2video/listenzahl Alles gemacht

---
+++ AUfgaben/Aufgabe3.2.8/Listenzahlen.py
@@ -0,0 +1,21 @@
+zahlenstrahl = [4, 7, 2, 9, 1, 5, 3,]
+
+#erster teil
+print(F'Die Länge der liste: {len(zahlenstrahl)}')
+print(F'Maximale Zahl: {max(zahlenstrahl)}')
+print(F'Minimalste Zahl: {min(zahlenstrahl)}')
+#erster test ging alles gut
+
+#gibt es eine funktion für denn durchschnitt?
+#Der Durchschnitt ist die Summe aller Zahlen geteilt durch die Anzahl der Zahlen – nicht einfach durch 2.
+durchschnitt = sum(zahlenstrahl) / len(zahlenstrahl)
+print(F'Der Durchschnittliche wert beträgt: {durchschnitt}')
+#sollte wohl auch vorwärts zeigen um ein rückwärts zu zeigen
+print(F'Vorwärts {zahlenstrahl}')
+print(F'Rückwärts {list(reversed(zahlenstrahl))}') #zahlenstrahl.reverse() ging nicht und hat mir wert NULL ausgegeben
+
+#habe hilfe gebraucht für die for klausel internetsuche hat geholfen
+#Modulo hier bei Zahl kann ich andere variablennamen wählen 
+#zahl gewählt weil das ein zahlenstrahl ist und das logisch also zahlen sind
+geradezahlen = [zahl for zahl in zahlenstrahl if zahl % 2 == 0]
+print(F'der neue Zahlenstrahl: {geradezahlen}')
---
+++ AUfgaben/Testlauf2.py
@@ -0,0 +1,6 @@
+#numbers = [3, 5, 2, 'Katzen']
+#print(numbers)
+
+names = ['Samuele', 'Rafael', 'Martin', 'Johanna', 'Nico', 4, 8.2]
+
+print(names[6])
