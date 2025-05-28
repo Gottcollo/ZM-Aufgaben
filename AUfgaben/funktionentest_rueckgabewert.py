d618c206673cd570bb825e29176577378ce24844
Gottcollo <gottcollo@gmail.com>
2025-05-28 11:12:26 +0200

    fehlercheck

--- AUfgaben/funktionentest_rueckgabewert.py
+++ AUfgaben/funktionentest_rueckgabewert.py
@@ -1,6 +1,6 @@
 def say_hello(first_name, last_name):
-    print('Hallo ' + first_name + ' ' + last_name)
+    print(print('Hallo ' + first_name + ' ' + last_name))
     print('Willkomen zurück')
 
-print(say_hello('Sam', 'Will'))
+print(type(say_hello('Sam', 'Will')))
 print('Test')
