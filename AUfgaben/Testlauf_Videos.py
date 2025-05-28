28a8638165b806419b6955ca39f59b29986f7ab5
Gottcollo <gottcollo@gmail.com>
2025-05-28 13:28:49 +0200

    ERstesObject(Car)gemacht_Sortiert

--- AUfgaben/Testlauf_Videos.py
+++ AUfgaben/Testlauf_Videos.py
@@ -1,5 +1,23 @@
-for element in [1, 8, 6, 15, 88]:
-    print(element)
+class Car:
+    def __init__(self):
+        self.car_brand = None
+        self.horse_power = None
+        self.color = None
 
-for elemental in range(5, 10, 2):
-    print(elemental)
+car1 = Car()
+print(car1.car_brand)
+car1.car_brand = 'Audi'
+car1.horse_power = 255
+car1.color = 'Blau'
+print(car1.car_brand)
+print(car1.color)
+print(car1.horse_power)
+
+car2 = Car()
+print(car2.car_brand)
+car2.car_brand = 'BMW'
+car2.horse_power = 255
+car2.color = 'Schwarz'
+print(car2.car_brand)
+print(car2.color)
+print(car2.horse_power)
